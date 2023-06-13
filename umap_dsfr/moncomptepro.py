5fd3e3dab8ab48d6768bc47a86500c0d06308638
Yohan Boniface <yohanboniface@free.fr>
2023-06-13 21:13:04 +0200

    Working MonComptePro social-auth class configuration

--- umap_dsfr/moncomptepro.py
+++ umap_dsfr/moncomptepro.py
@@ -2,4 +2,7 @@ from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 
 class MonComptePro(OpenIdConnectAuth):
-    name = 'moncomptepro'
+    name = "moncomptepro"
+    DEFAULT_SCOPE = ["openid", "profile", "email", "organizations"]
+    USERNAME_KEY = "sub"
+    EXTRA_DATA = ["id_token", "refresh_token", "organizations", "phone_number", "job", ("sub", "id")]
