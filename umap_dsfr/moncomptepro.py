8c15eb6f51d7e1cede0d579200231515a63aa800
Yohan Boniface <yohanboniface@free.fr>
2023-06-16 07:51:21 +0200

    Only allow login for public entities

--- umap_dsfr/moncomptepro.py
+++ umap_dsfr/moncomptepro.py
@@ -6,3 +6,10 @@ class MonComptePro(OpenIdConnectAuth):
     DEFAULT_SCOPE = ["openid", "profile", "email", "organizations"]
     USERNAME_KEY = "sub"
     EXTRA_DATA = ["id_token", "refresh_token", "organizations", "phone_number", "job", ("sub", "id")]
+
+    def auth_allowed(self, response, details):
+        for org in response.get("organizations", []):
+            if org["is_collectivite_territoriale"] or org["is_service_public"]:
+                # TODO should we check for is_external ?
+                return True
+        return False
