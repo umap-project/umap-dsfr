85c71391040aaf93e39e0156db514923fd7e626f
Yohan Boniface <yohanboniface@free.fr>
2023-07-27 12:30:43 +0200

    MonComptePro: switch from organizations scope to organization
    Change requested by MonComptePro

--- umap_dsfr/moncomptepro.py
+++ umap_dsfr/moncomptepro.py
@@ -3,13 +3,19 @@ from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 class MonComptePro(OpenIdConnectAuth):
     name = "moncomptepro"
-    DEFAULT_SCOPE = ["openid", "profile", "email", "organizations"]
+    DEFAULT_SCOPE = ["openid", "profile", "email", "organization"]
     USERNAME_KEY = "sub"
-    EXTRA_DATA = ["id_token", "refresh_token", "organizations", "phone_number", "job", ("sub", "id")]
+    EXTRA_DATA = [
+        "id_token",
+        "refresh_token",
+        "organization",
+        "phone_number",
+        "job",
+        ("sub", "id"),
+    ]
 
     def auth_allowed(self, response, details):
-        for org in response.get("organizations", []):
-            if org["is_collectivite_territoriale"] or org["is_service_public"]:
-                # TODO should we check for is_external ?
-                return True
+        if response["is_collectivite_territoriale"] or response["is_service_public"]:
+            # TODO should we check for is_external ?
+            return True
         return False
